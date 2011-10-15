8529fc055b50ec03fc973a285f9f8813d5d41593
Adrian Lang <mail@adrianlang.de>
2011-10-16 00:11:54 +0200

    Reject invalid compasses

--- kompass.js
+++ kompass.js
@@ -46,11 +46,28 @@ function get_kompass(kompass_getters, user, ncb_kompasshandler) {
     // Prepend default getter
     kompass_getters.unshift(get_static_kompass);
 
-    lib.untilValue(kompass_getters, function (getter, callback) {
-        getter(user, callback);
+    lib.untilValue(kompass_getters, function (getter, ncb_callback) {
+        getter(user, function (err, res) {
+            var reject = false;
+            if (res) {
+                if ([-10, 0, 10].indexOf(res.ec) !== -1 &&
+                    [-10, 0, 10].indexOf(res.soc) !== -1) {
+                    reject = 'boring';
+                } else if (res.ec < -10 || res.ec > 10 || res.soc < -10 || res.ec > 10) {
+                    reject = 'out-of-range';
+                }
+                if (reject) {
+                    err = 'Rejecting ' + user + '’s ' + reject + ' compass ' +
+                          '(ec: ' + lib.numForOutput(res.ec) + ' – ' +
+                          'soc: ' + lib.numForOutput(res.soc) + ')';
+                    res = null;
+                }
+            }
+            ncb_callback(err, res);
+        });
     }, function (errs, res) {
         if (errs) {
-            console.warn('Failed to get compass data for ' + user + ' (' + errs.map(function (v) { return '"' + v + '"'; }).join(',') + ')');
+            console.warn('Failed to get compass data for ' + user + ':\n' + errs.map(function (v) { return "  - " + v; }).join('\n'));
             errs = 'Failed to get compass data for ' + user;
         }
         ncb_kompasshandler(errs, res);
